1b534ad23409bed78b563471e4793dba6bc6ec4f
lucas barra <lucastbarra@hotmail.com>
2021-09-12 20:10:32 -0300

    config react-query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,8 @@ import {
     Tbody,
     Td,
     Text,
-    useBreakpointValue
+    useBreakpointValue,
+    Spinner
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine } from "react-icons/ri";
@@ -20,18 +21,21 @@ import { useEffect } from "react";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/SideBar";
+import { useQuery } from 'react-query';
 
 export default function UserList() {
+    const { data, isLoading, error } = useQuery('users', async () => {
+        const response = await fetch('http://localhost:3000/api/users')
+        const data = await response.json()
+
+        return data;
+    })
+
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
     });
-
-    useEffect(() => {
-        fetch('http://localhost:3000/api/users')
-            .then(response => response.json())
-            .then(data => console.log(data))
-    }, [])
 
     return (
         <Box>
@@ -57,35 +61,47 @@ export default function UserList() {
                         </Link>
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color="gray.300" width="8" >
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuário</Th>
-                                {isWideVersion && <Th>Data de cadastro</Th>}
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados dos usuários.</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color="gray.300" width="8" >
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        {isWideVersion && <Th>Data de cadastro</Th>}
 
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Lucas Barra</Text>
-                                        <Text fontSize="sm" color="gray.300">lucastbarra@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>22 de agosto, 2021</Td>}
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    <Tr>
+                                        <Td px={["4", "4", "6"]}>
+                                            <Checkbox colorScheme="pink" />
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">Lucas Barra</Text>
+                                                <Text fontSize="sm" color="gray.300">lucastbarra@hotmail.com</Text>
+                                            </Box>
+                                        </Td>
+                                        {isWideVersion && <Td>22 de agosto, 2021</Td>}
 
-                            </Tr>
-                        </Tbody>
-                    </Table>
+                                    </Tr>
+                                </Tbody>
+                            </Table>
 
-                    <Pagination />
+                            <Pagination />
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
