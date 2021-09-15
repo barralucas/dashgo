36daabc112fc0c43936a0c7e4a5654e3544d2f10
lucas barra <lucastbarra@hotmail.com>
2021-09-15 20:20:33 -0300

    listing users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,20 +17,32 @@ import {
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine } from "react-icons/ri";
-import { useEffect } from "react";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/SideBar";
 import { useQuery } from 'react-query';
+import { useEffect } from "react";
 
 export default function UserList() {
     const { data, isLoading, error } = useQuery('users', async () => {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
 
-        return data;
-    })
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        });
 
+        return users;
+    })
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -83,19 +95,23 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td px={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">Lucas Barra</Text>
-                                                <Text fontSize="sm" color="gray.300">lucastbarra@hotmail.com</Text>
-                                            </Box>
-                                        </Td>
-                                        {isWideVersion && <Td>22 de agosto, 2021</Td>}
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>{user.createdAt}</Td>}
 
-                                    </Tr>
+                                            </Tr>
+                                        )
+                                    })}
                                 </Tbody>
                             </Table>
 
