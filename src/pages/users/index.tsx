edc664c3123ddb36ae1f8ab745a1431c82963a07
lucas barra <lucastbarra@hotmail.com>
2021-09-21 11:52:19 -0300

    change page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
     Box,
     Button,
@@ -20,12 +21,11 @@ import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/SideBar";
-import { useQuery } from 'react-query';
-import { api } from "../../services/api";
 import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-    const { data, isLoading, isFetching, error } = useUsers();
+    const [page, setPage] = useState(1);
+    const { data, isLoading, isFetching, error } = useUsers(page);
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -82,7 +82,7 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    {data.map(user => {
+                                    {data.users.map(user => {
                                         return (
                                             <Tr key={user.id}>
                                                 <Td px={["4", "4", "6"]}>
@@ -103,9 +103,9 @@ export default function UserList() {
                             </Table>
 
                             <Pagination
-                                totalCountOfRegisters={200}
-                                currentPage={12}
-                                onPageChange={()=> { }}
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}
                             />
                         </>
                     )}
